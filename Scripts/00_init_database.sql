fd09f969c0c5c49ad10c7e331d514361234d2cbf
Naveen Surla <naveensurla1111@gmail.com>
2025-04-20 13:35:58 +0530

    Update 00_init_database.sql

--- Scripts/00_init_database.sql
+++ Scripts/00_init_database.sql
@@ -8,9 +8,8 @@ Script Purpose:
 	
 Step : 1 - I have created a new schema as below :
 
+	    CREATE SCHEMA `datawarehouse` ;
 
-CREATE SCHEMA `datawarehouse` ;
-
-Step : 2   - I have used Table data import wizard to get the final data files stored as csv files; which evolved after transforming through the data architecture for this project which follows Medallion Architecture Bronze, Silver, and Gold lay
-Step : 3   - I have mentioned the file path and imported the data after selecting the clause "Drop table if exists".
-Step : 4   - And i did the same with the rest of the data files and got my data imported properly.
+Step : 2 - I have used Table data import wizard to get the final data files stored as csv files; which evolved after transforming through the data architecture for this project which follows Medallion Architecture Bronze, Silver, and Gold lay
+Step : 3 - I have mentioned the file path and imported the data after selecting the clause "Drop table if exists".
+Step : 4 - And i did the same with the rest of the data files and got my data imported properly.
